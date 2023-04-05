49759bf7b7f0e427b8d2e73daa7566519a65695c
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-04-05 15:18:10 -0300

    Meu-Curriculo

---
+++ assets/js/habilidades.js
@@ -0,0 +1,18 @@
+const skills = document.querySelectorAll('.skill-box');
+const descricao = document.querySelector('.texto-descricao');
+
+const dataInicio = [
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    (new Date(2023, 3, 5)).getTime(), 
+                    ]
+const dataHoje = (new Date()).getTime();
+
+
+
+
